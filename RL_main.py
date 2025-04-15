da19e19354509fc1a85b6df4213049df05c962d0
markoy1928 <alexpmarquis@gmail.com>
2025-04-15 13:29:13 -0400

    Changed graph to plot rewards instead of time

--- RL_main.py
+++ RL_main.py
@@ -129,21 +129,29 @@ def select_action(state):
 
 
 episode_durations = []
+episode_rewards = []
 
 def plot_durations(show_result=False):
     plt.figure(1)
     durations_t = torch.tensor(episode_durations, dtype=torch.float)
+    rewards_t = torch.tensor(episode_rewards, dtype=torch.int32)
     if show_result:
         plt.title('Result')
     else:
         plt.clf()
         plt.title('Training...')
     plt.xlabel('Episode')
-    plt.ylabel('Duration')
-    plt.plot(durations_t.numpy())
+    # plt.ylabel('Duration')
+    # plt.plot(durations_t.numpy())
+    plt.ylabel('Reward Score')
+    plt.plot(rewards_t.numpy())
     # Take 100 episode averages and plot them too
-    if len(durations_t) >= 100:
-        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+    # if len(durations_t) >= 100:
+    #     means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+    #     means = torch.cat((torch.zeros(99), means))
+    #     plt.plot(means.numpy())
+    if len(rewards_t) >= 100:
+        means = rewards_t.unfold(0, 100, 1).mean(1).view(-1)
         means = torch.cat((torch.zeros(99), means))
         plt.plot(means.numpy())
 
@@ -240,6 +248,7 @@ for i_episode in range(num_episodes):
 
         if done:
             episode_durations.append(t + 1)
+            episode_rewards.append(reward)
             plot_durations()
             break
 
